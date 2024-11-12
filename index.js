6f8c5b6354233e305161f246ccfeb79da824dc00
Vishwa Badrinadh Padala <vishwabadrinadh.padala@gmail.com>
2024-11-12 20:08:56 +0530

    cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173' })); // Allow requests from the React frontend
+app.use(cors({ origin: 'https://e-learn-full-stack-frontend.onrender.com' })); // Allow requests from the React frontend
 app.use(express.json()); // Parse JSON requests
 app.use(express.urlencoded({ extended: true })); // Parse form submissions
 app.set('view engine', 'ejs'); // Set EJS as templating engine
